671e02cf0d1ec7d6dd7f2d6c13b2b1dd6b56b88d
Tyson Sorensen <tysonsorensen3@gmail.com>
2016-01-13 17:11:27 -0700

    adds project boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,124 @@
+'use strict';
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var htmlmin = require('gulp-htmlmin');
+var express = require('express');
+var livereload = require('gulp-livereload');
+var please = require('gulp-pleeease');
+var plumber = require('gulp-plumber');
+var lib = require('bower-files')({
+  overrides: {
+    bootstrap: {
+      main: [
+        'dist/js/bootstrap.js',
+        'dist/css/bootstrap.css',
+        'dist/fonts/*'
+      ]
+    },
+    textAngular: {
+      main: [
+        'dist/textAngular.css',
+        'dist/textAngular-rangy.min.js',
+        'dist/textAngular-sanitize.js',
+        'dist/textAngularSetup.js',
+        'dist/textAngular.js',
+      ]
+    }
+  }
+});
+
+
+gulp.task('default', [
+  'fonts',
+  'scripts',
+  'styles',
+  'static',
+  'templates'
+]);
+
+gulp.task('watch', [
+  'fonts',
+  'scripts.watch',
+  'styles.watch',
+  'static.watch',
+  'templates.watch',
+  'server',
+  'livereload'
+]);
+
+
+gulp.task('scripts', function () {
+  return gulp.src(lib.ext('js').files.concat('src/scripts/**/*.js'))
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('build/js'));
+});
+//watcher
+gulp.task('scripts.watch', ['scripts'], function () {
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+});
+
+
+gulp.task('styles', function () {
+  return gulp.src(lib.ext('css').files.concat('src/styles/**/*.css'))
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.min.css'))
+    .pipe(please())
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('build/css'));
+});
+//watcher
+gulp.task('styles.watch', ['styles'], function () {
+  gulp.watch('src/styles/**/*.css', ['styles']);
+});
+
+
+gulp.task('static', function () {
+  return gulp.src('src/static/**/*')
+    .pipe(plumber())
+    .pipe(gulp.dest('build'));
+});
+//watcher
+gulp.task('static.watch', ['static'], function () {
+  gulp.watch('src/static/**/*', ['static']);
+});
+
+gulp.task('fonts', function () {
+  return gulp.src(lib.ext(['eot', 'svg', 'ttf', 'woff', 'woff2']).files)
+    .pipe(plumber())
+    .pipe(gulp.dest('build/fonts'));
+});
+
+
+gulp.task('templates', function () {
+  return gulp.src('src/templates/**/*.html')
+    .pipe(plumber())
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('build'));
+});
+//watcher
+gulp.task('templates.watch', ['templates'], function () {
+  gulp.watch('src/templates/**/*.html', ['templates']);
+});
+
+//startup server on localhost
+gulp.task('server', function () {
+  require('./server');
+});
+
+
+gulp.task('livereload', function () {
+  livereload.listen();
+  gulp.watch('build/**/*', function (event) {
+    livereload.changed(event);
+  });
+});
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var blog = require('./routes/articles');
+
+//using multer for image/text uploads, supports multipart-data
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'src/static/images/uploads');
+  },
+  filename: function (req, file, cb) {
+    var name = file.originalname.split('.')[0];
+    var ext = file.mimetype.split('/')[1];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
+var upload = multer({ storage: storage });
+
+//API routes
+router.get('/articles', blog.findAll);
+router.post('/articles', upload.single('file'), blog.insertArticle);
+router.get('/articles/:id', blog.findById);
+router.post('/comments', upload.single(), blog.saveComment);
+
+//Use
+app.use('/api', router);
+app.use(express.static('build'));
+
+//Port it listens on
+var port = 8000;
+app.listen(port);
