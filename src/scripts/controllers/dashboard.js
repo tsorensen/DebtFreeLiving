84357d7eba979225364c317cb6d9fb51f930ee64
Tyson Sorensen <TysonS@morinda.com>
2016-02-29 16:20:58 -0700

    updated isLoggedIn function

--- src/scripts/controllers/dashboard.js
+++ src/scripts/controllers/dashboard.js
@@ -8,13 +8,9 @@ angular
     function(auth, $location) {
 
       //if no one is logged in, redirect to login page
-      auth.isLoggedIn().then(function(isLoggedIn) {
-        console.log('here is the auth stuff: ');
-        console.log(isLoggedIn);
-        if (!isLoggedIn) {
+      if(!auth.isLoggedIn()) {
           $location.url('/login');
-        }
-      });
+      }
 
     },
 
