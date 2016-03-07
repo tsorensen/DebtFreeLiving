5490935aac0dee6f529a298c776605c8e6ab73e2
Tyson Sorensen <TysonS@morinda.com>
2016-03-07 10:51:24 -0700

    adds log that shows what port the server is listening on

--- server.js
+++ server.js
@@ -41,4 +41,6 @@ app.use(express.static('build'));
 
 //Port it listens on
 var port = 8000;
-app.listen(port);
+app.listen(port, function() {
+  console.log('\n    Server listening on port ' + port + '\n');
+});
