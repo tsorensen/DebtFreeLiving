5e98db660d33fe3a4a5ecc2a85460bc17cef40d3
Tyson Sorensen <TysonS@morinda.com>
2016-03-18 14:34:32 -0600

    adds login check for comments section on articles, adds submit/retrieve comments functions

--- src/scripts/controllers/article.js
+++ src/scripts/controllers/article.js
@@ -2,23 +2,43 @@ angular
   .module('ArticleController', [
     'blogApp.articles',
     'renderHtmlFilter',
+    'blogApp.auth',
   ])
   .controller('ArticleController', [
     'articles',
+    'auth',
     '$route',
     '$routeParams',
     '$filter',
-    function(articles, $route, $routeParams, $filter) {
+    '$timeout',
+    function(articles, auth, $route, $routeParams, $filter, $timeout) {
       var self = this;
       self.id = $routeParams.id;
       self.content = [];
       self.articles = [];
       self.categories = [];
+      self.comments = [];
+      self.savingComment = false;
+      self.isLoggedIn = false;
+      self.user = {};
+      self.inputs = {};
 
       function getArticle() {
         articles.read(self.id)
           .then(function(article) {
             self.content = article;
+          })
+          .then(function() {
+            articles.getComments(self.id)
+              .then(function(comments) {
+                self.comments = comments;
+              })
+              .catch(function(error) {
+                console.log("Error in retrieving comments: ", error);
+              });
+          })
+          .catch(function(error) {
+            console.log('Error in retreiving article data: ', error);
           });
       }
 
@@ -49,6 +69,27 @@ angular
           });
       }
 
+      //get user data so we know if they are logged in and can comment or not
+      function setupCommentSection() {
+        auth.isLoggedIn()
+          .then(function(user) {
+              self.isLoggedIn = true;
+              self.user = user;
+          })
+          .then(function(res) {
+            auth.getCurrentUser()
+              .then(function(userData) {
+                  self.inputs.name = userData.firstName + ' ' + userData.lastName;
+              })
+              .catch(function(error) {
+                console.log('Error in retreiving user data: ', error);
+              });
+          })
+          .catch(function(error) {
+            console.log('Error in retreiving logged in data: ', error);
+          });
+      }
+
       function formatCategories() {
         self.categories = $filter('unique')(self.categories);
 
@@ -72,24 +113,47 @@ angular
       resetCommentForm();
 
       self.submit = function (data) {
+        self.savingComment = true;
+        self.saveCommentError = null;
+        self.saveCommentSuccess = null;
+
+        if(!data.comment) {
+          self.savingComment = false;
+          self.saveCommentError = 'Please fill in the comment field.';
+          return;
+        }
+
         var comment = {
           id: self.id,
           name: data.name,
           content: data.comment,
+          articleName: self.content.title,
         };
 
-        articles.createComment(comment)
-          .then(function() {
-            $route.reload();
+        return articles.createComment(comment)
+          .then(function(res) {
+            $timeout(function() {
+              self.saveCommentSuccess = "Your comment has been submitted successfully.";
+            });
+
+            $timeout(function() {
+              self.savingComment = false;
+              $route.reload()
+            }, 3000);
             console.log('success');
           })
-          .catch(function(res) {
-            console.log('There was an error: ');
-            console.log(res.data);
+          .catch(function(error) {
+            self.saveCommentError = 'There was an error submitting your comment.  Please try again.';
+            console.log('There was an error: ', error);
+            console.log(error.code);
+            self.savingComment = false;
           });
       };
 
       getRecentArticles();
       getArticle();
+      if(auth.isLoggedIn()) {
+        setupCommentSection();
+      }
     },
   ]);
