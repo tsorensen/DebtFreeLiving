06a083a377a4267773b9f4d79218453b578977a5
Tyson Sorensen <TysonS@morinda.com>
2016-03-21 16:27:20 -0600

    adds new adminLink directive

---
+++ src/scripts/directives/adminLink.js
@@ -0,0 +1,47 @@
+angular
+.module('adminLinkDirective', [
+  'blogApp.auth'
+])
+.directive('adminLink', [
+  'auth',
+  '$location',
+  '$timeout',
+  '$compile',
+  function(auth, $location, $timeout, $compile) {
+    return {
+      restrict: 'AE',
+      scope: {},
+      link: function(scope, elem, attrs) {
+
+        //puts either "Signup|Login" in the menu OR
+        //"Hello, <firstName>|Logout" of user that is signed in
+        scope.setAdminLink = function() {
+          var loggedIn = auth.isLoggedIn();
+
+          if(loggedIn) {
+            auth.getCurrentUser()
+              .then(function(user) {
+                console.log('current user data: ');
+                console.log(user);
+                if(user.administrator === true) {
+                  elem.html('<a href="/#/admin">Admin</a>');
+                } else {
+                  elem.html('');
+                }
+              });
+            } else {
+              elem.html('');
+            }
+          $compile(elem.contents())(scope);
+        }
+
+        //Load adminLink for first time
+        scope.setAdminLink();
+
+        //Listen to login/logout changes, update menu.
+        scope.$on('auth-userLoginChange', scope.setAdminLink);
+      },
+
+    };
+  }
+]);
