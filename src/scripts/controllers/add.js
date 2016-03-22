fd49c6dfcf9260ad011e0e68e155c83f863952bd
Tyson Sorensen <TysonS@morinda.com>
2016-03-22 12:59:59 -0600

    adds better data validation for adding articles

--- src/scripts/controllers/add.js
+++ src/scripts/controllers/add.js
@@ -31,7 +31,14 @@ angular
         self.saveArticleError = null;
         self.saveArticleSuccess = null;
 
+        if(!data.title || !data.author || !data.content) {
+          self.saveArticleError = 'The Title, Author, and Body fields are required.';
+          self.savingArticle = false;
+          return;
+        }
+
         var image = $scope.myFile || '';
+        data.category = data.category ? data.category.toLowerCase() : '';
         console.log('here is the image: ');
         console.log(image);
         console.log('article data: ');
@@ -40,23 +47,20 @@ angular
         var article = {
           title: data.title,
           author: data.author,
-          category: data.category.toLowerCase(),
+          category: data.category,
           content: data.content
         };
 
         return articles.create(article, image)
           .then(function(res) {
-            console.log(res);
             self.saveArticleSuccess = 'This article has been saved successfully.';
             $timeout(function() {
               self.savingArticle = false;
               $location.url('/admin');
             }, 3000);
-            console.log('success');
           })
           .catch(function(error) {
-            console.log('There was an error: ');
-            console.log(error);
+            console.log('There was an error: ', error);
             console.log(error.code);
             self.saveArticleError = 'There was an error saving the article: ' + error;
             self.savingArticle = false;
