19267d5445a624522a321e9d23b9a452c875771d
Tyson Sorensen <TysonS@morinda.com>
2016-03-30 14:48:56 -0600

    adds html layout and default content for now

--- src/scripts/controllers/dashboard.js
+++ src/scripts/controllers/dashboard.js
@@ -1,11 +1,31 @@
 angular
   .module('DashboardController', [
-
+    'chart.js',
   ])
   .controller('DashboardController', [
+    '$scope',
     '$location',
-    function($location) {
+    '$timeout',
+    function($scope, $location, $timeout) {
+      var self = this;
 
+      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+      $scope.series = ['Series A', 'Series B'];
+      $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ];
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
+
+      // Simulate async data update
+      $timeout(function () {
+        $scope.data = [
+          [28, 48, 40, 19, 86, 27, 90],
+          [65, 59, 80, 81, 56, 55, 40]
+        ];
+      }, 3000);
 
     },
 
