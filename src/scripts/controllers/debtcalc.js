8bdc1cee136c35753df61fd2562d25d1c26c08ef
Tyson Sorensen <tysonsorensen3@gmail.com>
2016-04-10 16:18:18 -0600

    adds Micah's pr changes, adds graph code

--- src/scripts/controllers/debtcalc.js
+++ src/scripts/controllers/debtcalc.js
@@ -6,24 +6,24 @@ angular
     '$scope',
     'auth',
     '$firebaseArray',
-    function($scope, auth, $firebaseArray) {
-
+    '$timeout',
+    function($scope, auth, $firebaseArray, $timeout) {
       var ref = new Firebase("https://resplendent-fire-5282.firebaseio.com/");
-
+​
       $scope.initList = [{},{}];
-
+​
       auth.isLoggedIn()
         .then(function(user) {
             $scope.user = user;
             $scope.userData = $firebaseArray(ref.child("plans").child($scope.user.uid));
-
+​
             $scope.userData.$loaded()
             .then(function(){
               if($scope.userData.length > 0){
                   $scope.initList = $scope.userData;
-                  $scope.calcLoans();
+                  $scope.displayLoans();
               }
-
+​
               if($scope.userData.length > 2){
                 $scope.showRemoveBtn = true;
               }
@@ -32,13 +32,13 @@ angular
         .catch(function(error) {
           console.log('Error in retreiving logged in data: ', error);
         });
-
+​
       var table;
-
+​
       $scope.columns = [];
-
+​
       var showTable = true;
-
+​
       $scope.showRemoveBtn = false;
       $scope.showForm = true;
       $scope.showPlan = false;
@@ -47,28 +47,35 @@ angular
       $scope.errorMessage = null;
       $scope.errorAmount = null;
 
+      //graph variables
+      $scope.graphLabels = [];
+      $scope.data = [];
+      $scope.graphData = [];
+      $scope.graphSeries = [];
+      $scope.graphPie = [];
+​
       $scope.addLoans = function(){
         $scope.initList.push(
           {}
         )
-
+​
         if($scope.initList.length > 2 || $scope.userData > 2){
           $scope.showRemoveBtn = true;
         }
       };
-
+​
       $scope.removeLoan = function(k){
         $scope.initList.splice(k, 1);
-
+​
         if($scope.initList.length < 3){
           $scope.showRemoveBtn = false;
         }
       };
-
+​
       $scope.editPlan = function(){
         $scope.showForm = true;
       }
-
+​
       $scope.resetPlan = function(){
         BootstrapDialog.show({
             type: BootstrapDialog.TYPE_DANGER,
@@ -90,7 +97,7 @@ angular
             }]
         });
       }
-
+​
       $scope.clearAll = function(){
         $scope.initList = [{},{}];
         $scope.loanList = [];
@@ -101,27 +108,9 @@ angular
         //table.draw();
         $scope.showPlan = false;
       };
-
-      $scope.calcLoans = function(){
-
-        $scope.isError = false;
-        $scope.errorMessage = [];
-
-        console.log($scope.userData);
-        for(i = 2; i < $scope.userData.length; i++){
-          $scope.userData.$remove($scope.userData[i]);
-        }
-        console.log($scope.userData);
-
-        for(i = 0; i < $scope.initList.length; i++){
-          $scope.userData.$add($scope.initList[i]);
-        }
-
-        //Clear the loanList array and the final output array
-        $scope.loanList = [];
-        $scope.finalOutput = [];
-        $scope.columns = [];
-
+​
+      $scope.displayLoans = function(){
+​
         for(j=0; j < $scope.initList.length; j++){
           $scope.loanList.push(
             {
@@ -133,65 +122,92 @@ angular
               staticPayment: parseFloat($scope.initList[j].payment),
               paidOff: false
             })
-
+​
           //Arrange all objects in loanList by interest rate by highest to lowest
           $scope.loanList.sort(sortInt);
           function sortInt(a, b) {
           return b.calcIntRate-a.calcIntRate;
           };
         }
-
+​
         termCalc($scope.loanList);
         createColumns($scope.loanList);
-
+​
         if(table){ table.destroy(); }
-
+​
         table = $('#outputTable').DataTable( {
             "searching": false,
             "ordering": false,
-            responsive: true,
-            "stripeClasses": [ 'strip1', 'strip2', 'strip3' ],
+            "stripeClasses": [ 'strip1', 'strip2' ],
             data: $scope.finalOutput,
             columns: $scope.columns
         } );
 
+
+        //get graph data from finalOutput, create data and labels arrays,
+        //remove months from data array
+        for(var i = 0; i < 6; i++) {
+          $scope.data[i] = $scope.finalOutput[i];
+          $scope.graphLabels.push($scope.data[i][0]);
+
+          //don't need month for data array
+          $scope.data[i].splice(0, 1);
+          for(var j = 0; j < $scope.data[i].length; j++) {
+            //remove $ sign
+            $scope.data[i][j] = parseFloat($scope.data[i][j].substring(1));
+          }
+        }
+
+        for(var i = 0; i < $scope.loanList.length; i++) {
+          //fill in series data
+          $scope.graphSeries.push($scope.loanList[i].calcDesc);
+          //get pie totals
+          $scope.graphPie.push($scope.initList[i].balance);
+
+          $scope.graphData.push([]);
+          for(var j = 0; j < $scope.data.length; j++) {
+            $scope.graphData[i].push($scope.data[j][i]);
+          }
+        }
+
+​
         $scope.showPlan = true;
         $scope.showForm = false;
-
+​
         function termCalc(loanList){
-
+​
           var totalBalance = 0;
           var totalSnowball = 0;
           var paidOff = false;
           var monthlyTotal = 0;
           var monthsToAdd = 1;
           var lastMonthsTotalBalance;
-
+​
           for(i=0; i < loanList.length; i++){
             totalBalance += loanList[i].calcBalance;
           }
-
+​
           for(i=0; i < loanList.length; i++){
             monthlyTotal += $scope.loanList[i].staticPayment;
           }
-
+​
           while(totalBalance > 0){
-
+​
             if(totalBalance == lastMonthsTotalBalance){
               totalBalance = 0;
             }
-
+​
             $scope.finalOutput.push(
               [moment($scope.loanList[0].date, "MMMM YYYY").add(monthsToAdd, 'months').format("YYYY MMM")]
             )
-
+​
             monthsToAdd++;
-
+​
             recalculatePayments();
             function recalculatePayments(){
-
+​
               var newMonthContainer = 0;
-
+​
               //Determine the default payment amount for every item in the current month
               for(i=0; i < loanList.length; i++){
                 //If the payment is higher than the balance, this months payment is only equal to the balance.
@@ -200,16 +216,16 @@ angular
                   loanList[i].paidOff = true;
                 }
               }
-
+​
               //Determine what is left of the monthly total BEFORE snowball payments are applied
               for(i=0; i < loanList.length; i++){
                 //Now that the initial monthly payments have been calculated, find the difference between
                 //the "new total" and the original monthly total.
                 newMonthContainer += loanList[i].calcPayment;
               }
-
+​
               finalPayment = true;
-
+​
               //Determine if this is the final payments
               for(i=0; i < loanList.length; i++){
                 if(!loanList[i].paidOff){
@@ -227,50 +243,50 @@ angular
                 }
               }
             };
-
+​
             var placeholderArray = [];
-
+​
             for(i=0; i < loanList.length; i++){
               //Make a payment
-
+​
               lastMonthsTotalBalance = totalBalance;
-
+​
               var monthInt = loanList[i].calcIntRate / 1200;
               var monthlyIntPmt = loanList[i].calcBalance * monthInt;
               var principal = loanList[i].calcPayment - monthlyIntPmt;
-
+​
               if(loanList[i].calcPayment > loanList[i].calcBalance){
                 loanList.calcBalance = 0;
               }
-
+​
               if(loanList[i].calcPayment > totalBalance){
                 totalBalance = 0;
               }
-
+​
               $scope.finalOutput[$scope.finalOutput.length - 1].push(
                 "$" + loanList[i].calcPayment.toFixed(2)
               )
-
+​
               placeholderArray.push(
                 "$" + loanList[i].calcPayment.toFixed(2)
               )
-
+​
               loanList[i].calcBalance -= principal.toFixed(2);
               totalBalance -= principal.toFixed(2);
-
+​
               if (loanList[i].calcBalance < 0.01){
                 loanList[i].calcBalance = 0;
               }
-
+​
               if (totalBalance < 0.01) {
                 totalBalance = 0;
               }
             }
-
+​
             for (i=0; i < placeholderArray.length; i++){
-
+​
               var isCompleted = true;
-
+​
               if(placeholderArray[i] != "$0.00"){
                 isCompleted = false;
               }
@@ -278,7 +294,7 @@ angular
             if(isCompleted){
               totalBalance = 0;
             }
-
+​
             if(monthsToAdd > 600){
               $scope.errorMessage.push("With the current debt information provided, the number of months required to pay off " +
               "this debt has exceeded 600. Please consider increasing payments.");
@@ -291,11 +307,11 @@ angular
           }
         };
       function createColumns(loanList){
-
+​
         $scope.columns.push(
           { title: "MONTH" }
         );
-
+​
         for(j=0; j < loanList.length; j++){
           $scope.columns.push(
             { title: loanList[j].calcDesc }
@@ -303,5 +319,54 @@ angular
         }
       };
     };
+
+
+    //graph options - temp donut data
+    $scope.barColors = [
+      { //1
+        "fillColor": "#97BBCD",
+        "strokeColor": "#97BBCD",
+      },
+      { //2
+        "fillColor": "#DCDCDC",
+        "strokeColor": "#DCDCDC",
+      },
+      { //3
+        "fillColor": "#F7464A",
+        "strokeColor": "#F7464A",
+      },
+      { //4
+        "fillColor": "#46BFBD",
+        "strokeColor": "#46BFBD",
+      },
+      { //5
+        "fillColor": "#FDB45C",
+        "strokeColor": "#FDB45C",
+      },
+      { //6
+        "fillColor": "#949FB1",
+        "strokeColor": "#949FB1",
+      },
+      { //7
+        "fillColor": "#4D5360",
+        "strokeColor": "#4D5360",
+      },
+      { //8
+        "fillColor": "#D4B89E",
+        "strokeColor": "#D4B89E",
+      },
+      { //9
+        "fillColor": "#E0B7D1",
+        "strokeColor": "#E0B7D1",
+      },
+      { //10
+        "fillColor": "#5487D4",
+        "strokeColor": "#5487D4",
+      }];
+
+    $scope.pieLabels = ["Credit Card", "Auto Loan", "Student Loans", 'another', 'another', 'another', 'another', 'another', 'another', 'another'];
+    $scope.pieData = [300, 500, 100, 300, 500, 100, 300, 500, 100, 300];
+
+
   }
 ]);
